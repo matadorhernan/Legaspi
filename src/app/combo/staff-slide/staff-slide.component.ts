68bf15a812c9e7981b6ba685828feace6d914d2b
Miguel Romero <miguelesvida@gmail.com>
2019-08-17 16:28:06 -0500

    add combo staff slide

--- src/app/combo/staff-slide/staff-slide.component.ts
+++ src/app/combo/staff-slide/staff-slide.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ComboService } from '../../services/combo.service';
 
 @Component({
   selector: 'app-staff-slide',
@@ -6,10 +7,46 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./staff-slide.component.css']
 })
 export class StaffSlideComponent implements OnInit {
+  public waiter = false;
+  public cook = false;
+  public security = false;
+  public mariachi = false;
 
-  constructor() { }
+  private key = 'legaspi-staff';
 
-  ngOnInit() {
+  constructor(private comboService: ComboService) {
+    this.comboService.isCurrentValid.next(true);
+
+    let savedSelected = this.comboService.getSession(this.key);
+    if (savedSelected) {
+      this.waiter = savedSelected.waiter
+      this.cook = savedSelected.cook
+      this.security = savedSelected.security
+      this.mariachi = savedSelected.mariachi
+    }
   }
 
+  ngOnInit() {}
+
+  selectOperations(inputChoice: string) {
+    switch (inputChoice) {
+      case 'waiter': this.waiter = !this.waiter
+        break;
+      case 'cook': this.cook = !this.cook
+        break;
+      case 'security': this.security = !this.security
+        break;
+      case 'mariachi': this.mariachi = !this.mariachi
+        break;
+    }
+
+    let staff = {
+      waiter: this.waiter,
+      cook: this.cook,
+      security: this.security,
+      mariachi: this.mariachi
+    }
+
+    this.comboService.setSession(this.key, staff)
+  }
 }
