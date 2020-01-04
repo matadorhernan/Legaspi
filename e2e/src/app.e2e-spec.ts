30bf39ce39627450b77108ab87cefc023ba88fb2
Miguel Angel Romero Hernandez <miguelromereado@outlook.com>
2020-01-03 21:31:37 -0600

    Add files via upload

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to legaspi!');
+    expect(page.getTitleText()).toEqual('CorpLegaspiAngular app is running!');
   });
 
   afterEach(async () => {
